a09213ced877e8654fd272fd695d758a01415362
lgolikova <milagoul@gmail.com>
2022-09-19 19:18:00 +0300

    done

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     let notes1 = "";
-    console.log(localStorage.length);
     for (let i = 0; i < localStorage.length; i++) {
         let key = localStorage.key(i);
         if (localStorage.getItem(key) != null) {
@@ -8,7 +7,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
     }
     notes.innerHTML = notes1;
-
 });
 
 let date = document.querySelector("#date");
@@ -16,7 +14,6 @@ let note = document.querySelector("#note");
 let btn = document.querySelector("#save");
 let notes = document.querySelector(".notes");
 let btnClear = document.querySelector("#clear");
-
 
 btn.addEventListener("click", () => {
     if (localStorage.getItem(`${date.value}`) == null) {
@@ -32,14 +29,6 @@ btn.addEventListener("click", () => {
 
     date.value = '';
     note.value = '';
-
-        
-    // if (localStorage.getItem(date) == null) {
-    //     localStorage.setItem(date, `$date.value`)
-    // }
-    // if (localStorage.getItem(note) == null) {
-    //     localStorage.setItem(note, `$note.value`)
-    // }
 })
 
 btnClear.addEventListener("click", () => {
